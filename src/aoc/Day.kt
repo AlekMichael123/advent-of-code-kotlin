26e750d63a00c0bad7a830f0b304332f81b73b29
alekmichael123 <alekmichael2345@gmail.com>
2024-08-12 23:08:33 -0700

    initalized the beginning to Kotlin AOC!!!

---
+++ src/aoc/Day.kt
@@ -0,0 +1,6 @@
+package aoc
+
+interface Day {
+  public fun part1(input: String);
+  public fun part2(input: String);
+}
---
+++ src/aoc/year2023/Day4.kt
@@ -0,0 +1,13 @@
+package aoc.year2023
+
+import aoc.Day
+
+class Day4 : Day {
+  override fun part1(input: String) {
+    TODO("Not yet implemented")
+  }
+
+  override fun part2(input: String) {
+    TODO("Not yet implemented")
+  }
+}
