95d9ab7c4bf2b751924f6570b0c155fc1ab21c89
AlekMichael123 <alekmichael2345@gmail.com>
2024-08-14 00:45:10 -0700

    Day 4: set up parsing input

--- src/aoc/year2023/Day4.kt
+++ src/aoc/year2023/Day4.kt
@@ -4,17 +4,19 @@ import aoc.Day
 
 class Day4 : Day {
   override fun part1(input: String) {
-    println(input)
-    println("Part 1")
+    val numbers = splitInput(input)
   }
 
   override fun part2(input: String) {
     println("Part 2")
   }
 
-  private fun splitInput(input: String) {
-    val (_, winningNumbers, myNumbers) = input.split(":", "|")
-    println(winningNumbers)
-    println(myNumbers)
-  }
+  private fun splitInput(input: String): List<Pair<List<String>, List<String>>> =
+    input.lines().map { line ->
+      val (_, winningNumbers, myNumbers) = line.split(":", "|")
+
+      winningNumbers.trim().split(" ").filter { it.isNotEmpty() } to
+      myNumbers.trim().split(" ").filter { it.isNotEmpty() }
+    }
+
 }
