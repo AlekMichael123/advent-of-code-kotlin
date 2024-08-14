fe985e44c61e3e85bd690f95503c43fba3131251
AlekMichael123 <alekmichael2345@gmail.com>
2024-08-14 00:13:51 -0700

    test commit, trying to push

--- src/aoc/year2023/Day4.kt
+++ src/aoc/year2023/Day4.kt
@@ -4,10 +4,17 @@ import aoc.Day
 
 class Day4 : Day {
   override fun part1(input: String) {
+    splitInput(input)
     println("Part 1")
   }
 
   override fun part2(input: String) {
     println("Part 2")
+  }
+
+  private fun splitInput(input: String) {
+    val (_, winningNumbers, myNumbers) = input.split(":", "|")
+    println(winningNumbers)
+    println(myNumbers)
   }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,11 +1,16 @@
 import aoc.getYearSolution
+import java.io.File
 
 fun main(args: Array<String>) {
   val (year, day) = args
+  val input = readFileDirectlyAsText("input.txt")
   val yearSolutions = getYearSolution(year.toInt())
   val solution = yearSolutions?.getSolution(day.toInt())
   solution?.let {
-    it.part1("")
-    it.part2("")
+    it.part1(input)
+    it.part2(input)
   }
 }
+
+fun readFileDirectlyAsText(fileName: String): String
+    = File(fileName).readText(Charsets.UTF_8)
