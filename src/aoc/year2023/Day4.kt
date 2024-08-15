b754def7541d28ce13b2e3c802b3127ff9181cb1
AlekMichael123 <alekmichael2345@gmail.com>
2024-08-14 17:08:13 -0700

    Day 4: Working on part 2 spent too long trying to figure out what the question wasn't asking me

--- src/aoc/year2023/Day4.kt
+++ src/aoc/year2023/Day4.kt
@@ -5,22 +5,30 @@ import aoc.Day
 class Day4 : Day {
   override fun part1(input: String) {
     println("Part 1")
-    val numbers = splitInput(input)
-    val result = numbers.fold(0) { total, (winning, myNumbers) ->
+    val result = playGame(splitInput(input))
+    println("Total score is $result")
+  }
+
+  override fun part2(input: String) {
+    println("Part 2")
+    val cards = splitInput(input)
+    val copies = IntArray(cards.size)
+    val result = cards.foldIndexed(0) {  i, total, (winningNumbers, myNumbers) ->
+      total
+    }
+    println("Total scorecards is $result")
+  }
+
+  private fun playGame(cards: List<Pair<List<String>, List<String>>>): Int =
+    cards.fold(0) { total, (winningNumbers, myNumbers) ->
       total + myNumbers.fold(0) { acc, num ->
-        if (winning.contains(num)) {
+        if (winningNumbers.contains(num)) {
           if (acc == 0) 1 else acc * 2
         } else {
           acc
         }
       }
     }
-    println("Total score is $result")
-  }
-
-  override fun part2(input: String) {
-    println("Part 2")
-  }
 
   private fun splitInput(input: String): List<Pair<List<String>, List<String>>> =
     input.lines().map { line ->
