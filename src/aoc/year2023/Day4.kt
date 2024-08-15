755a04bab66a6304c93eec9acdaf1e9f2ac1876d
AlekMichael123 <alekmichael2345@gmail.com>
2024-08-14 23:16:43 -0700

    2023 - Day 4: Part 2 done! Was busy but came back and Aced it

--- src/aoc/year2023/Day4.kt
+++ src/aoc/year2023/Day4.kt
@@ -6,17 +6,14 @@ class Day4 : Day {
   override fun part1(input: String) {
     println("Part 1")
     val result = playGame(splitInput(input))
-    println("Total score is $result")
+    println("Total score: $result")
   }
 
   override fun part2(input: String) {
     println("Part 2")
     val cards = splitInput(input)
-    val copies = IntArray(cards.size)
-    val result = cards.foldIndexed(0) {  i, total, (winningNumbers, myNumbers) ->
-      total
-    }
-    println("Total scorecards is $result")
+    val totalCards = countCards(cards)
+    println("Total cards: $totalCards")
   }
 
   private fun playGame(cards: List<Pair<List<String>, List<String>>>): Int =
@@ -28,6 +25,19 @@ class Day4 : Day {
           acc
         }
       }
+    }
+
+  private fun countCards(cards: List<Pair<List<String>, List<String>>>): Int =
+    cards.foldIndexed(IntArray(cards.size)) { i, instances, (winningNumbers, myNumbers) ->
+      instances[i]++ // count cards[i] itself
+      val totalWins = myNumbers.count { winningNumbers.contains(it) }
+      for (index in i+1..i+totalWins) {
+        if (index >= cards.size) break
+        instances[index] += instances[i]
+      }
+      instances
+    }.fold(0) { totalCards, instance ->
+      totalCards + instance
     }
 
   private fun splitInput(input: String): List<Pair<List<String>, List<String>>> =
