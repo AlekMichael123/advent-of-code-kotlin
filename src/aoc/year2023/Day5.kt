5b401e195950691ad4e78c0c89b208c8bf568203
AlekMichael123 <alekmichael2345@gmail.com>
2024-08-16 22:26:58 -0700

    2023 Day 5 Part 2: FINALLY figured it out! It turns out the problem is completely worded wrong! I'll clean up later

--- src/aoc/year2023/Day5.kt
+++ src/aoc/year2023/Day5.kt
@@ -12,7 +12,6 @@ class Day5 : Day {
    * it should be source destination but WHATEVER
    */
 
-
   override fun part1(input: String) {
     println("Part 1")
     val (
@@ -49,13 +48,77 @@ class Day5 : Day {
 
   override fun part2(input: String) {
     println("Part 2")
+    val (
+      initialSeeds,
+      seedToSoil,
+      soilToFertilizer,
+      fertilizerToWater,
+      waterToLight,
+      lightToTemperature,
+      temperatureToHumidity,
+      humidityToLocation,
+    ) = parseInput(input)
+
+    val initialSeed = seedRangesStringToRanges(initialSeeds[0])
+    val seedToSoilRanges = rangesStringToRanges(seedToSoil)
+    val soilToFertilizerRanges = rangesStringToRanges(soilToFertilizer)
+    val fertilizerToWaterRanges = rangesStringToRanges(fertilizerToWater)
+    val waterToLightRanges = rangesStringToRanges(waterToLight)
+    val lightToTemperatureRanges = rangesStringToRanges(lightToTemperature)
+    val temperatureToHumidityRanges = rangesStringToRanges(temperatureToHumidity)
+    val humidityToLocationRanges = rangesStringToRanges(humidityToLocation)
+
+    val seedToSoilRangesMap = createMapBasedOffRangesFromRanges(initialSeed, seedToSoilRanges)
+    val soilToFertilizerRangesMap = createMapBasedOffRangesFromRanges(flattenRangesMap(seedToSoilRangesMap), soilToFertilizerRanges)
+    val fertilizerToWaterRangesMap = createMapBasedOffRangesFromRanges(flattenRangesMap(soilToFertilizerRangesMap), fertilizerToWaterRanges)
+    val waterToLightRangesMap = createMapBasedOffRangesFromRanges(flattenRangesMap(fertilizerToWaterRangesMap), waterToLightRanges)
+    val lightToTemperatureRangesMap = createMapBasedOffRangesFromRanges(flattenRangesMap(waterToLightRangesMap), lightToTemperatureRanges)
+    val temperatureToHumidityRangesMap = createMapBasedOffRangesFromRanges(flattenRangesMap(lightToTemperatureRangesMap), temperatureToHumidityRanges)
+    val humidityToLocationRangesMap = createMapBasedOffRangesFromRanges(flattenRangesMap(temperatureToHumidityRangesMap), humidityToLocationRanges)
+
+    val closestAvailableIsland = flattenRangesMap(humidityToLocationRangesMap).filter { it.first != 0L }.minByOrNull { it.first }?.first
+    println("Closest available location is: " + (closestAvailableIsland ?: "None Available"))
   }
 
-  private fun createMapBasedOffRanges(initialValues: List<Long>, ranges: List<Pair<LongRange, LongRange>>) =
+  private fun createMapBasedOffRangesFromRanges(initialRanges:  List<Pair<Long, Long>>, ranges: List<Pair<Pair<Long, Long>, Pair<Long, Long>>>) =
+    initialRanges.associateWith { initialRange ->
+      val list = mutableListOf<Pair<Long, Long>>()
+      ranges.forEach { (destinationRange, sourceRange) ->
+        if (
+          !(initialRange.first <= sourceRange.second && sourceRange.first <= initialRange.second)
+        ) {
+          return@forEach
+        }
+
+        /**
+         * AN: WHY DOES PART 1 CONSIDER UNMAPPED VALUES BUT NOT PART 2??? I MUST BE STUPID BUT I THINK THIS PROBLEM
+         * WAS VERY POORLY WORDED!!!! Wasted two days on this because I was considering cases such as:
+         * [1,2,3]       -- init
+         *       [4,5,6] -- source
+         * [1,2,3]       -- mapped values
+         * but NOPE we just don't want these values at all. WTF?
+         */
+        val overlapFirst = sourceRange.first.coerceAtLeast(initialRange.first)
+        val overlapLast = sourceRange.second.coerceAtMost(initialRange.second)
+        if (overlapFirst < overlapLast) {
+          val offset = destinationRange.first - sourceRange.first
+          list.add(overlapFirst+offset to overlapLast+offset)
+        }
+
+
+      }
+      // add unmapped values from initialRange
+      if (list.isEmpty()) {
+        list.add(initialRange)
+      }
+      list
+    }
+
+  private fun createMapBasedOffRanges(initialValues: List<Long>, ranges: List<Pair<Pair<Long, Long>, Pair<Long, Long>>>) =
     initialValues.associateWith { value ->
       val list =  mutableListOf<Long>()
       ranges.forEach { (destinationRange, sourceRange) ->
-        if (value in sourceRange) {
+        if (value in (sourceRange.first .. sourceRange.second)) {
           // LOL first I was using indexOf which was slow, but then I wised up
           val index = value - sourceRange.first
           list.add(destinationRange.first + index)
@@ -65,13 +128,20 @@ class Day5 : Day {
       if (list.isEmpty()) {
         list.add(value) // if not mapped, map with same value
       }
-
       list
     }
 
+  private fun seedRangesStringToRanges(seedRanges: List<Long>) =
+    seedRanges
+      .mapIndexed { i, seedInitial ->
+        if (i % 2 != 0) null
+        else seedInitial to seedInitial + seedRanges[i + 1]
+      }
+      .filterNotNull()
+
   private fun rangesStringToRanges(rangesString: List<List<Long>>) =
     rangesString.map { (aStart, bStart, rangeLength) ->
-      (aStart until aStart + rangeLength) to (bStart until bStart + rangeLength)
+      (aStart to aStart + rangeLength - 1) to (bStart to bStart + rangeLength - 1)
     }
 
   private fun parseInput(input: String) =
@@ -91,5 +161,8 @@ class Day5 : Day {
       .map { line -> line.split("\n").map { it.split(" ").map(String::toLong) } }
 
   private fun flattenMap(map: Map<Long, MutableList<Long>>) =
+    map.values.toList().flatten()
+
+  private fun flattenRangesMap(map:  Map<Pair<Long, Long>, MutableList<Pair<Long, Long>>>) =
     map.values.toList().flatten()
 }
