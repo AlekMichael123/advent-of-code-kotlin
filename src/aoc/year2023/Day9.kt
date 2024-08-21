15c63101a0237e364a4f415e59530b4c465517a6
AlekMichael123 <alekmichael2345@gmail.com>
2024-08-20 22:40:52 -0700

    2023 Day 9: Part 2 complete, time to refactor

--- src/aoc/year2023/Day9.kt
+++ src/aoc/year2023/Day9.kt
@@ -5,31 +5,38 @@ import aoc.Day
 class Day9 : Day {
   override fun part1(input: String) {
     val data = parseInput(input)
-
     val result = data.fold(0) { acc, d ->
-      acc + calculateDifferences(d).fold(0) { total, diff -> total + diff }
+      acc + calculateDifferences(d, beginning = false).fold(0) { total, diff -> total + diff }
     }
-
     println("Total is $result")
   }
 
   override fun part2(input: String) {
-
+    val data = parseInput(input)
+    val result = data.fold(0) { acc, d ->
+      val differences = calculateDifferences(d, beginning = true)
+      var extrapolated = differences.last()
+      var i = differences.size - 2
+      while (i >= 0) {
+        extrapolated = differences[i--] - extrapolated
+      }
+      acc + extrapolated
+    }
+    println("Total is $result")
   }
 
-  private fun calculateDifferences(values: List<Int>): List<Int> {
-    val differences = mutableListOf<Int>(values.last())
+  private fun calculateDifferences(values: List<Int>, beginning: Boolean): List<Int> {
+    val differences = mutableListOf(if (beginning) values.first() else values.last())
     var currDifferences: List<Int> = values.mapIndexed { i, value ->
       if (i < values.size - 1) values[i + 1] - value
       else null
     }.filterNotNull()
 
     while (currDifferences.any { it != 0 }) {
-      differences.add(currDifferences.last())
+      differences.add(if (beginning) currDifferences.first() else currDifferences.last())
       currDifferences = currDifferences.mapIndexed { i, value ->
-        if (i < currDifferences.size - 1) {
+        if (i < currDifferences.size - 1)
           currDifferences[i + 1] - value
-        }
         else null
       }.filterNotNull()
     }
