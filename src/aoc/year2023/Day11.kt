f79811a4ce6371bd7a2186964b3f9d747967d1d0
AlekMichael123 <alekmichael2345@gmail.com>
2024-09-03 00:59:01 -0700

    2023 Day 11: Phew! part 2 done, fun recoding into a better memory performant solution

--- src/aoc/year2023/Day11.kt
+++ src/aoc/year2023/Day11.kt
@@ -1,24 +1,39 @@
 package aoc.year2023
-
 import aoc.Day
 import kotlin.math.abs
+
+typealias ExpandedUniverseInformation = Pair<List<Boolean>, List<Boolean>>
+
 class Day11 : Day {
   override fun part1(input: String) {
-    val result = getShortestPathsBetweenGalaxies(expandUniverse(parseInput(input)))
+    val result = getShortestPathsBetweenGalaxies(data = parseInput(input), expansionScale = 2)
     println("Sum of all shortest paths is $result")
   }
 
   override fun part2(input: String) {
+    val result = getShortestPathsBetweenGalaxies(data = parseInput(input), expansionScale = 1_000_000)
+    println("Sum of all shortest paths is $result")
   }
 
-  private fun getShortestPathsBetweenGalaxies(data: List<String>) =
+  private fun getShortestPathsBetweenGalaxies(data: List<String>, expansionScale: Int) =
     findGalaxies(data).let { galaxyPositions ->
-      galaxyPositions.mapIndexed { i, (i1, j1) ->
-        galaxyPositions.mapIndexed { j, (i2, j2) ->
-          if (i >= j) 0
-          else abs(i1 - i2) + abs(j1 - j2)
+      expandUniverse(data).let { (emptyRowIndices, emptyColIndices) ->
+        galaxyPositions.mapIndexed { i, (srcI, srcJ) ->
+          galaxyPositions.mapIndexed { j, (targetI, targetJ) ->
+            if (i >= j) 0
+            else {
+              val rows = if (srcI > targetI) srcI downTo targetI else srcI..targetI
+              val cols = if (srcJ > targetJ) srcJ downTo targetJ else srcJ..targetJ
+              var emptyAmt = 0
+
+              for (row in rows) if (emptyRowIndices[row]) emptyAmt++
+              for (col in cols) if (emptyColIndices[col]) emptyAmt++
+
+              abs(srcI - targetI) + abs(srcJ - targetJ) + (emptyAmt * expansionScale.dec())
+            }
+          }
         }
-      }.flatten().fold(0) { acc, sum -> acc + sum }
+      }.flatten().fold(0L) { acc, sum -> acc + sum }
     }
 
   private fun findGalaxies(data: List<String>) =
@@ -26,28 +41,17 @@ class Day11 : Day {
       row.mapIndexedNotNull { j, char -> (i to j).takeIf { char == '#' } }
     }.flatten()
 
-  private fun expandUniverse(data: List<String>): List<String> {
-    val rowExpansion = data.map { row ->
-      if (allEmptyRow(row))
-        listOf(row, ".".repeat(data.first().length))
-      else
-        listOf(row)
-    }.flatten()
+  private fun expandUniverse(data: List<String>): ExpandedUniverseInformation {
+    val emptyRowIndices = data.map { row -> isAllEmptyRow(row) }
+    val emptyColIndices = data.first().indices.map { j -> isAllEmptyCol(data, j) }
 
-    val emptyColsIndices = data.first().indices.map { j -> allEmptyCol(data, j) }
-
-    return rowExpansion.map { row ->
-      row.mapIndexed { j, char ->
-        if (emptyColsIndices[j]) ".".repeat(2)
-        else char
-      }.joinToString("")
-    }
+    return emptyRowIndices to emptyColIndices
   }
 
-  private fun allEmptyRow(row: String) =
+  private fun isAllEmptyRow(row: String) =
     row.all { it == '.' }
 
-  private fun allEmptyCol(data: List<String>, j: Int) =
+  private fun isAllEmptyCol(data: List<String>, j: Int) =
     data.all { it[j] == '.' }
 
   private fun parseInput(input: String) =
