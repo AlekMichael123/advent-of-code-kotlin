b213987f64237aaad818ba15735e01ffb092ad91
AlekMichael123 <alekmichael2345@gmail.com>
2024-09-04 19:29:42 -0700

    2023 Day 12: parseInput implemented

--- src/aoc/year2023/Day12.kt
+++ src/aoc/year2023/Day12.kt
@@ -4,8 +4,18 @@ import aoc.Day
 
 class Day12 : Day {
   override fun part1(input: String) {
+    val data = parseInput(input)
+    println(data)
   }
 
   override fun part2(input: String) {
   }
+
+  private fun parseInput(input: String) =
+    input
+      .lines()
+      .map {
+        val (record, sizes) = it.split(" ")
+        record to sizes.split(",").map(String::toInt)
+      }
 }
