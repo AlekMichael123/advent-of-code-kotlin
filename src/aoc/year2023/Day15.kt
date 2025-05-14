79c725b8354fbe43871309384f895368fc374c6e
AlekMichael123 <alekmichael2345@gmail.com>
2025-05-14 02:25:57 -0700

    2023 day 15 part 1 complete

--- src/aoc/year2023/Day15.kt
+++ src/aoc/year2023/Day15.kt
@@ -1,11 +1,25 @@
 package aoc.year2023
 
 import aoc.Day
+import kotlin.streams.toList
 
 class Day15 : Day {
   override fun part1(input: String) {
+    val data = parseInput(input)
+    val result = data.fold(0) { acc, str ->
+      acc + hash(str)
+    }
+    println("Result is $result")
   }
 
   override fun part2(input: String) {
   }
+
+  private fun hash(str: String) =
+    str.chars().toList().fold(0) { acc, char ->
+      ((acc + char) * 17) % 256
+    }
+
+  private fun parseInput(input: String) =
+    input.split(",")
 }
