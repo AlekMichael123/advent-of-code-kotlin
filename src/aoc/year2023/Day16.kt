edd8d02ae3019386c379485718d38ed9c2dffedd
AlekMichael123 <alekmichael2345@gmail.com>
2025-05-15 15:59:56 -0700

    2023 day 16 complete, ugly but works

--- src/aoc/year2023/Day16.kt
+++ src/aoc/year2023/Day16.kt
@@ -2,6 +2,9 @@ package aoc.year2023
 
 import aoc.Day
 
+/**
+ * SUPER bruteforce solution LOL
+ */
 class Day16 : Day {
   private enum class Directions(val direction: Pair<Int, Int>) {
     NORTH(-1 to 0),
@@ -14,29 +17,52 @@ class Day16 : Day {
 
   override fun part1(input: String) {
     val mirrors = parseInput(input)
-    val result = traceLight(mirrors).fold(0) { acc, visited -> acc + visited.count { it } }
+    val result = calcTotalEnergy(mirrors, startingPosition = 0 to 0, Directions.EAST)
     println("Result is $result")
   }
 
   override fun part2(input: String) {
+    val mirrors = parseInput(input)
+    var result = 0
+
+    mirrors.first().indices.forEach { j ->
+      result = result.coerceAtLeast(calcTotalEnergy(mirrors, 0 to j, Directions.SOUTH))
+      result = result.coerceAtLeast(calcTotalEnergy(mirrors, mirrors.lastIndex to j, Directions.NORTH))
+    }
+
+    mirrors.indices.forEach { i ->
+      result = result.coerceAtLeast(calcTotalEnergy(mirrors, i to 0, Directions.EAST))
+      result = result.coerceAtLeast(calcTotalEnergy(mirrors, i to mirrors.first().lastIndex, Directions.WEST))
+    }
+
+    println("Result is $result")
   }
 
-  private fun traceLight(mirrors: List<CharArray>): Array<BooleanArray> {
+  private fun calcTotalEnergy(mirrors: List<CharArray>, startingPosition: Pair<Int, Int>, startingDirection: Directions) =
+    traceLight(mirrors, startingPosition, startingDirection)
+      .fold(0) { acc, visited -> acc + visited.count { it } }
+
+  private fun traceLight(mirrors: List<CharArray>, startingPosition: Pair<Int, Int>, startingDirection: Directions): Array<BooleanArray> {
     val n = mirrors.size
     val m = mirrors.first().size
     val visited = Array(n) { BooleanArray(m )}
-    val lights = mutableListOf(Light(direction = Directions.EAST, position = 0 to 0))
+    val lights = mutableListOf(Light(direction = startingDirection, position = startingPosition))
     val visitedMap = mutableMapOf<Pair<Int, Int>, MutableList<Directions>>()
     val specialSpots = listOf('\\', '/', '|', '-')
+
     while (!lights.isEmpty()) {
       val (direction, position) = lights.removeFirst()
       val (y, x) = position
       val (dy, dx) = direction.direction
+
+      // pre calculate possible directions
       val northward = y + Directions.NORTH.direction.first to x + Directions.NORTH.direction.second
       val southward = y + Directions.SOUTH.direction.first to x + Directions.SOUTH.direction.second
       val westward = y + Directions.WEST.direction.first to x + Directions.WEST.direction.second
       val eastward = y + Directions.EAST.direction.first to x + Directions.EAST.direction.second
       val onward = y + dy to x + dx
+
+      // check if this pattern is already recorded
       if (specialSpots.contains(mirrors[y][x])) {
         if (visitedMap.contains(position) && visitedMap[position]!!.contains(direction)) {
           continue
@@ -45,63 +71,58 @@ class Day16 : Day {
           visitedMap[position]!!.add(direction)
         }
       }
+
       if (outOfBounds(y, x, n, m)) continue
+
       // mark space as visited
       visited[y][x] = true
+
       when (mirrors[y][x]) {
-        '|' -> if (direction == Directions.WEST || direction == Directions.EAST) {
-          if (!outOfBounds(northward.first, northward.second, n, m))
-            lights.add(Light(direction = Directions.NORTH, position = northward))
-          if (!outOfBounds(southward.first, southward.second, n, m))
-            lights.add(Light(direction = Directions.SOUTH, position = southward))
-        } else {
-          if (!outOfBounds(onward.first, onward.second, n, m))
-            lights.add(Light(direction = direction, position = onward))
-        }
-        '-' -> if (direction == Directions.NORTH || direction == Directions.SOUTH) {
-          if (!outOfBounds(westward.first, westward.second, n, m))
-            lights.add(Light(direction = Directions.WEST, position = westward))
-          if (!outOfBounds(eastward.first, eastward.second, n, m))
-            lights.add(Light(direction = Directions.EAST, position = eastward))
-        } else {
-          if (!outOfBounds(onward.first, onward.second, n, m))
-            lights.add(Light(direction = direction, position = onward))
-        }
-        '\\' -> when (direction) {
-          Directions.NORTH -> {
-            if (!outOfBounds(westward.first, westward.second, n, m))
-              lights.add(Light(direction = Directions.WEST, position = westward))
-          }
-          Directions.EAST -> {
+        '|' ->
+          if (direction == Directions.WEST || direction == Directions.EAST) {
+            if (!outOfBounds(northward.first, northward.second, n, m))
+              lights.add(Light(direction = Directions.NORTH, position = northward))
             if (!outOfBounds(southward.first, southward.second, n, m))
               lights.add(Light(direction = Directions.SOUTH, position = southward))
           }
-          Directions.SOUTH -> {
+          else if (!outOfBounds(onward.first, onward.second, n, m))
+              lights.add(Light(direction = direction, position = onward))
+        '-' ->
+          if (direction == Directions.NORTH || direction == Directions.SOUTH) {
+            if (!outOfBounds(westward.first, westward.second, n, m))
+              lights.add(Light(direction = Directions.WEST, position = westward))
             if (!outOfBounds(eastward.first, eastward.second, n, m))
               lights.add(Light(direction = Directions.EAST, position = eastward))
           }
-          Directions.WEST -> {
+          else if (!outOfBounds(onward.first, onward.second, n, m))
+              lights.add(Light(direction = direction, position = onward))
+        '\\' -> when (direction) {
+          Directions.NORTH ->
+            if (!outOfBounds(westward.first, westward.second, n, m))
+              lights.add(Light(direction = Directions.WEST, position = westward))
+          Directions.EAST ->
+            if (!outOfBounds(southward.first, southward.second, n, m))
+              lights.add(Light(direction = Directions.SOUTH, position = southward))
+          Directions.SOUTH ->
+            if (!outOfBounds(eastward.first, eastward.second, n, m))
+              lights.add(Light(direction = Directions.EAST, position = eastward))
+          Directions.WEST ->
             if (!outOfBounds(northward.first, northward.second, n, m))
               lights.add(Light(direction = Directions.NORTH, position = northward))
-          }
         }
         '/' -> when (direction) {
-          Directions.NORTH -> {
+          Directions.NORTH ->
             if (!outOfBounds(eastward.first, eastward.second, n, m))
               lights.add(Light(direction = Directions.EAST, position = eastward))
-          }
-          Directions.EAST -> {
+          Directions.EAST ->
             if (!outOfBounds(northward.first, northward.second, n, m))
               lights.add(Light(direction = Directions.NORTH, position = northward))
-          }
-          Directions.SOUTH -> {
+          Directions.SOUTH ->
             if (!outOfBounds(westward.first, westward.second, n, m))
               lights.add(Light(direction = Directions.WEST, position = westward))
-          }
-          Directions.WEST -> {
+          Directions.WEST ->
             if (!outOfBounds(southward.first, southward.second, n, m))
               lights.add(Light(direction = Directions.SOUTH, position = southward))
-          }
         }
         '.' ->
           if (!outOfBounds(onward.first, onward.second, n, m))
