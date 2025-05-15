cf26daba2013573ab3d8dbee602e41349beaac97
AlekMichael123 <alekmichael2345@gmail.com>
2025-05-15 01:22:18 -0700

    2023 day 15 complete

--- src/aoc/year2023/Day15.kt
+++ src/aoc/year2023/Day15.kt
@@ -13,6 +13,33 @@ class Day15 : Day {
   }
 
   override fun part2(input: String) {
+    val data = parseInput(input)
+    val boxes = MutableList(256) { mutableListOf<Pair<String, Int>>() }
+    data.forEach { value ->
+      if (value.contains("-")) {
+        val label = value.filter { it != '-'}
+        val hashCode = hash(label)
+        boxes[hashCode] = boxes[hashCode].filter { it.first != label }.toMutableList()
+      } else {
+        val (label, lengthStr) = value.split("=")
+        val length = lengthStr.toInt()
+        val hashCode = hash(label)
+        if (boxes[hashCode].any { it.first == label }) {
+          val i = boxes[hashCode].indexOfFirst { it.first == label }
+          boxes[hashCode][i] = boxes[hashCode][i].copy(second = length)
+        } else {
+          boxes[hashCode].add(label to length)
+        }
+      }
+    }
+
+    val result = boxes.foldIndexed(0) { i, acc, items ->
+      acc + items.foldIndexed(0) { j, acc, (_, length) ->
+        acc + ((i + 1) * (j + 1) * length)
+      }
+    }
+
+    println("Result is $result")
   }
 
   private fun hash(str: String) =
